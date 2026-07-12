cf3bfd853e71d1987edd69fae547ca6d9baf65a1
yio0905 <yio0905@wufai.tc.edu.tw>
2026-07-12 23:37:08 +0800

    V1.0

---
+++ config.js
@@ -0,0 +1,19 @@
+// 課表系統前端設定檔案
+const CONFIG = {
+  title: "臺中市立霧峰農工 115年度暑期輔導課表查詢系統",
+  schoolName: "霧峰農工",
+  timetableCsv: "./timetable_wufa.csv",
+  homeroomsJson: "./homerooms_wufa.json",
+  periodTimes: {
+    "1": "08:00 - 08:50",
+    "2": "09:10 - 10:00",
+    "3": "10:10 - 11:00",
+    "4": "11:05 - 11:55",
+    "5": "13:00 - 13:50",
+    "6": "14:00 - 14:50",
+    "7": "15:00 - 15:50"
+  }
+};
+if (typeof module !== 'undefined') {
+  module.exports = CONFIG;
+}
